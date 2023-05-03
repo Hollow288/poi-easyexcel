82af9fb6ab60f8772f44d574067d9be3dbb7c636
hollow <571339274@qq.com>
2023-05-03 16:17:13 +0800

    poi和easyExcel

--- poi/src/main/java/com/hollow/easy/DemoData.java
+++ poi/src/main/java/com/hollow/easy/DemoData.java
@@ -2,6 +2,7 @@ package com.hollow.easy;
 
 import com.alibaba.excel.annotation.ExcelIgnore;
 import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.annotation.format.DateTimeFormat;
 import lombok.Data;
 
 import java.util.Date;
@@ -16,6 +17,10 @@ public class DemoData {
     @ExcelProperty("字符串标题")
     private String string;
 
+//    /**
+//     * 这里用string 去接日期才能格式化。我想接收年月日格式
+//     */
+//    @DateTimeFormat("yyyy年MM月dd日HH时mm分ss秒")
     @ExcelProperty("日期标题")
     private Date date;
 
